dc5441109513a9d6ab7888bd1f0b1201df4d471f
Magda Sidor <magda_si@wp.pl>
2018-11-08 20:44:51 +0100

    project init

---
+++ dziennik-treningowy/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Dziennik Treningowy
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
